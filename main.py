21d47446c41b664bdc4be4fb1adc8eeabf39cefd
Gurjas Singh <gurjassingh92@gmail.com>
2020-11-13 23:34:00 +0530

    added only using list

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from fastapi import FastAPI ## fast api
+from fastapi import FastAPI, Form ## fast api
 import uvicorn ## uvicorn server to run the app
 
-app = FastAPI() ## run app in debug mode
+app = FastAPI(debug=True) ## run app in debug mode
+
+allBooks = []
 
 @app.get("/") ## get route
-@app.get("/homepage") ## get route
 def read_root(): ## function that gets called for "/" route
+    print("root")
     return {"message": "Hello World"} ## return json response by default
 
-@app.get("/{name}") ## parameterized route
+@app.get("/user/{name}") ## parameterized route
 def value(name: str): ## parameter, datatype
     return {"message" : "hello user {}".format(name)}
+
+@app.post("/addBook")
+def addNewBook(name: str = Form(...), description : str = Form(...)):
+    allBooks.append({
+        'name': name,
+        'description': description
+    })
+    return {"message": "new book added with name {}".format(name)}
+
+@app.get("/viewBooks")
+def viewBooks():
+    return {"message": allBooks}
 
 if __name__ == "__main__":
     uvicorn.run(
